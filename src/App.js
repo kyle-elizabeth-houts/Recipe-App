911577c6d67076de00be920443ef9038153b2eae
Kyle Houts <kyle.elizabeth.houts@gmail.com>
2019-11-11 12:59:30 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RecipeSearcher from './RecipeSearcher';
+
+function App() {
+  return (
+    <div className="App">
+      <RecipeSearcher />
+    </div>
+  );
+}
+
+export default App;
